8a7b300e882ff0d101acee5e17f912dc86b07dc5
Korchevskaja Julyja <julija.korchevskaja@privatbank.ua>
2021-10-25 20:46:00 +0300

    create new task and show all tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,6 +9,37 @@
 
     </div>
     <a href = "{{route('tasks.create')}}" class="btn btn-success"><i class="fa fa-plus"></i> Create new task</a>
-
-    <!-- TODO: Текущие задачи -->
+    <!-- Текущие задачи -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                All tasks
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <!-- Заголовок таблицы -->
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <!-- Тело таблицы -->
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <!-- Имя задачи -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+                            <td>
+                               Delete{{$task->id}}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
